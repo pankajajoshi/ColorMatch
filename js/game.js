d700c287c9ac1f3d935ca83027948d111888a656
pankajajoshi <joshi.pankaja@gmail.com>
2023-07-02 11:57:16 -0400

    Adding condition if no maching blocks found
    Added end modal game as game ove for no matching found
    
    todo moving over the hanging coloums when matching blocks are removed

--- js/game.js
+++ js/game.js
@@ -20,6 +20,13 @@ const updateScore = () => {
       openEndGameModal();
       displayEndGameMessage();
       //updateScore();
+    } else {
+        console.log('score is less than 1500, next check if user can play further');
+        //check if any same blocks are remaining, if not show game over
+        if(!areThereAnyMatchingBlocks()) {
+           openEndGameModal();
+           displayEndGameMessage();
+        }
     }
   };
   
@@ -34,16 +41,15 @@ const updateScore = () => {
     winnerSound.play();
     if (document.querySelectorAll(".block").length === 0) {
       endGameModalTitle.textContent = `You've cleared all Blocks! You Win !! Your Score : ${score}`;
-      //endGameModalTitle.innerHTML = `You've cleared all Blocks! <span style="font-size: 24px; font-weight: bold;">You Win !</span> Your Score: ${score}`;
-      endGameModalTitle.style.color = "rgb(14, 52, 164)";
-      endGameModalTitle.style.fontSize = "24px";
-      endGameModalTitle.style.textAlign = "center";
     } else if(score >= 1500) {
       endGameModalTitle.textContent = `Awesome! you earned 1500 or more points, on to next level!! `;
-      endGameModalTitle.style.color = "rgb(14, 52, 164)";
-      endGameModalTitle.style.fontSize = "24px";
-      endGameModalTitle.style.textAlign = "center";
+    } else {
+        // this no more matches available to play further, end the game
+        endGameModalTitle.textContent = `There are no matching blocks! Game over! `;
     }
+    endGameModalTitle.style.color = "rgb(14, 52, 164)";
+    endGameModalTitle.style.fontSize = "24px";
+    endGameModalTitle.style.textAlign = "center";
   };
 
 
@@ -56,6 +62,35 @@ for (var i = 0; i < m; i++) {
 
 let blocksToFill=0;
 
+function areThereAnyMatchingBlocks() {
+
+    var doesAnyRowHaveMatchingBlocks = false;
+    for(var x=0;x<m-1;x++){
+        for(var col=0;col<n;col++){
+            //console.log('x:', x, 'col:', col, ' box[x][col]:', box[x][col], ' box[x+1][col]: ', box[x+1][col]);
+            if( (box[x][col] == box[x+1][col]) &&  box[x][col] != 'white') {
+                doesAnyRowHaveMatchingBlocks = true;
+                break;
+            }
+        }
+    }
+
+    var doesAnyColumnHaveMatchingBlocks = false;
+    for(var y=0;y<n-1;y++){
+        for(var row=0;row<n;row++){
+           // console.log('y:', y, 'row:', row, ' box[row][y]:', box[row][y], ' box[row][y+1]: ', box[row][y+1]);
+            if( (box[row][y] == box[row][y+1]) &&  box[row][y] != 'white') {
+                doesAnyColumnHaveMatchingBlocks = true;
+                break;
+            }
+        }
+    }
+
+   console.log('doesAnyRowHaveMatchingBlocks: ', doesAnyRowHaveMatchingBlocks);
+   console.log('doesAnyColumnHaveMatchingBlocks: ', doesAnyColumnHaveMatchingBlocks);
+
+    return doesAnyRowHaveMatchingBlocks || doesAnyColumnHaveMatchingBlocks;
+}
 function createBlocksInGrid(){
     for (var x = 0; x < m; x++) {
         box.push([]);
@@ -101,30 +136,6 @@ function blockClicked(eventObj) {
     populateSameColorNeighbors(eventObj.currentTarget.color, eventObj.currentTarget.row, eventObj.currentTarget.col, sameColorNeighbors);
     console.log(sameColorNeighbors);
     assignShakeStyleToBoxes(eventObj.currentTarget.color, sameColorNeighbors);
-}
-
-function slideBlocks1() {
-   // switch bottom white blocks with top colored blocks -- use columns only, rows remain as is
-   var elems = document.querySelectorAll(".white");
-   console.log(elems, elems.length);
-   [].forEach.call(elems, function (el) {
-    let currCoords = el.id.split("_"); // id="0_0" -> ["0", "0"]
-    let r = parseInt(currCoords[0]);
-    let c = parseInt(currCoords[1]);
-    r=r-1;
-    if(r>-1){
-        console.log(r+"_"+c);
-        let topEle = document.getElementById(r+"_"+c);
-        console.log(topEle);
-        let temp = topEle.className;
-        topEle.className=el.className;
-        el.className = temp;
-        // remove shaking
-       // topEle.classList.remove("shaking");
-      //  el.classList.remove("shaking");
-    }
-   });
-   clearShakingClass();
 }
 
 function slideBlocks() {
@@ -217,73 +228,17 @@ function clearClass(className) {
 
 function populateSameColorNeighbors(color, row, col, arr) {
     console.log('box[row][col]', box[row][col], ' finding color: ', color, ' found at loc: ', row, col);
-    if(row>=5 || col>=5 || row<0 || col<0) return false;
+    if(row>=5 || col>=5 || row<0 || col<0) return;
     if(box[row][col] != color){
         console.log('no matching color ', color, 'at location: ', row, col);
-       return false;
+       return;
     }
     arr.push([row, col]);
     
     populateSameColorNeighbors(color, row+1, col, arr);
     populateSameColorNeighbors(color, row, col+1, arr);
     //populateSameColorNeighbors(color, row-1, col, arr);
-   // populateSameColorNeighbors(color, row, col-1, arr);
-}
-
-//creating create blocks
-function createBlocks() {
-  const gridBox = document.getElementById("grid-box");
-  if (!gridBox) {
-    console.error("Cannot find element with ID 'grid-box'");
-    return;
-  }
-
-  createBlocksInGrid();
-
-  for (let i = 0; i < 30; i++) {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    const block = document.createElement("div");
-    block.classList.add("block", randomColor, "falling"); // block yellow falling
-    block.addEventListener("click", () => {
-      if (!oneBlockClicked) {
-        oneBlockClicked = block;
-        block.style.opacity = "0.35";
-        let blockSound = new Audio("blockcrush.wav");
-        blockSound.play();
-      } else if (!otherBlockClicked && oneBlockClicked !== block) {
-        otherBlockClicked = block;
-        block.style.opacity = "0.35";
-        if (oneBlockClicked.classList[1] === otherBlockClicked.classList[1]) {
-          const blocksToRemove = document.querySelectorAll(
-            `.block.${oneBlockClicked.classList[1]}`
-          );
-          blocksToRemove.forEach((blockToRemove) => {
-            console.log(blockToRemove);
-           // blockToRemove.remove();
-            score += 100;
-          });
-          oneBlockClicked = null;
-          otherBlockClicked = null;
-          scoreBoard.textContent = `Score: ${score}`;
-          checkWinner();
-        } else {
-          setTimeout(() => {
-            oneBlockClicked.style.opacity = "1";
-            otherBlockClicked.style.opacity = "1";
-            oneBlockClicked = null;
-            otherBlockClicked = null;
-          }, 250);
-        }
-      }
-    });
-   // gridBox.appendChild(block);
-  }
-
- 
-
-
-
-  //addRestartListener();
+    //populateSameColorNeighbors(color, row, col-1, arr);
 }
 
 createBlocksInGrid();
